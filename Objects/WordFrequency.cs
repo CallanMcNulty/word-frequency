73d06e6858f04d2215baa8a9a10c22e0fdd0ec96
Callan McNulty <jabberwocky222@gmail.com>
2016-07-08 08:44:23 -0700

    Create project file structure

---
+++ Objects/WordFrequency.cs
@@ -0,0 +1,7 @@
+namespace weekdayFinder
+{
+  public class Finder
+  {
+    //Object goes here
+  }
+}
---
+++ Tests/WordFrequencyTests.cs
@@ -0,0 +1,9 @@
+using Xunit;
+
+namespace WordFrequency
+{
+  public class WordFrequencyTests
+  {
+    //Tests go here
+  }
+}
