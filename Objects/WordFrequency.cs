515d57a27bf1a959d37dc2564acd88cc7e27b27a
Callan McNulty <jabberwocky222@gmail.com>
2016-07-08 09:36:30 -0700

    Add code to pass fifth test, will now successfully count multiple character words without including sub-words also added tests for words on the ends of the searched string and search terms that are longer than the searched string

--- Objects/WordFrequency.cs
+++ Objects/WordFrequency.cs
@@ -8,9 +8,10 @@ namespace WordFrequency
     public static int CountRepeats(string searchTerm, string searchedString)
     {
       int count = 0;
-      for(int i=0; i<searchedString.Length; i++)
+      int termLength = searchTerm.Length;
+      for(int i=0; i<searchedString.Length-termLength+1; i++)
       {
-        string chunk = searchedString.Substring(i,1);
+        string chunk = searchedString.Substring(i,termLength);
         char preChunk = i==0 ? ' ' : searchedString[i-1];
         char postChunk = i+chunk.Length>=searchedString.Length ? ' ' : searchedString[i+chunk.Length];
         bool chunkIsWord = Array.IndexOf(wordChars,preChunk)==-1 && Array.IndexOf(wordChars,postChunk)==-1;
--- Tests/WordFrequencyTests.cs
+++ Tests/WordFrequencyTests.cs
@@ -43,5 +43,35 @@ namespace WordFrequency
       //Assert
       Assert.Equal(expected, repeatNumber);
     }
+    //////Extra Test Not in Specs: Will it find words on the ends?/Will it find multiple words?////
+    [Fact]
+    public void CountRepeats__a_ahotdogama__2()
+    {
+      //Arrange/Act
+      int repeatNumber = RepeatCounter.CountRepeats("a","a hot dog am a");
+      int expected = 2;
+      //Assert
+      Assert.Equal(expected, repeatNumber);
+    }
+
+    [Fact]
+    public void CountRepeats__am_Iamaprogrammer__1()
+    {
+      //Arrange/Act
+      int repeatNumber = RepeatCounter.CountRepeats("a","I am a programmer.");
+      int expected = 1;
+      //Assert
+      Assert.Equal(expected, repeatNumber);
+    }
+    //////Extra Test Not in Specs: Will it work for search terms longer than searched string////
+    [Fact]
+    public void CountRepeats__programmer_I__0()
+    {
+      //Arrange/Act
+      int repeatNumber = RepeatCounter.CountRepeats("programmer","I");
+      int expected = 0;
+      //Assert
+      Assert.Equal(expected, repeatNumber);
+    }
   }
 }
